470d4794473401a187a56d868c8b9f8402116296
nischay-code <nischay.patel.code@gmail.com>
2024-01-26 17:38:55 +0530

    Creating a Reducer & Action Creator in Redux

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1 className="m-3">Deposit/Withdraw Money</h1>
+      <button className="btn btn-dark m-3">-</button>
+      Update Balance
+      <button className="btn btn-dark m-3">+</button>
+    </div>
+  );
+};
+
+export default Shop;
