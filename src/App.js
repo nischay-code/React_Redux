a3af64b3609e18a24dcd0b5d80c48f78a6b89181
nischay-code <nischay.patel.code@gmail.com>
2024-03-07 13:36:13 +0530

    Redux Action

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Navbar from "./components/Navbar";
-import Shop from "./components/Shop";
-
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="container">
-        <Shop />
-      </div>
+      <h1>Hello</h1>
     </>
   );
 }
