f11f4cb8cd5524b7bb4fe01599e1f8843187fe56
nischay-code <nischay.patel.code@gmail.com>
2024-03-07 14:11:34 +0530

    Redux Store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Provider } from "react-redux";
+import store from "./redux/Store";
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <h1>Hello</h1>
-    </>
+    </Provider>
   );
 }
 
